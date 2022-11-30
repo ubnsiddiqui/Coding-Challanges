47a3fd6ee8e7f932b8219aa82032ac0b335320db
Muhammad Usama Siddiqui <usiddiqui14@gmail.com>
2022-12-01 01:41:54 +0500

    Check if a number inlisthas unique occurances

--- uniq_occurences.py
+++ uniq_occurences.py
@@ -1,12 +1,15 @@
 class Solution:
     def uniqueOccurrences(self, arr):
-        lst = arr
-        arr_len = len(lst)
-        uniq_arr = list(set(lst))
-        uniq_arr_len = len(uniq_arr)
-        if uniq_arr_len != arr_len:
+        countOfChars = dict()
+        for e in arr:
+            if countOfChars.get(e):
+                countOfChars[e] +=1
+            else:
+                countOfChars[e] = 1
+        countDictList = list(countOfChars.values())
+        if len(countDictList) == len(set(countDictList)):
             return 'true'
-        elif uniq_arr_len == arr_len:
+        else:
             return 'false'
 s = Solution()
 print(s.uniqueOccurrences([1,2]))
